5c9724ffba76cce5a7dad70ebf2d1b4fb0006e8a
rose <rosedorleans1@gmail.com>
2021-11-20 13:16:16 +0100

    feat: modal

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+// Index 
+
 $(function() {
-    $('#pokedex-logo').hover(function() {
-        $('#pokedex-logo').css('background-image', "url('img/pokedex-logo.png')");
-        $('#body').css('background-image', "url('img/pokeball-paper.png')");
-        $('#body').css('background-position', "center");
-    }, function() {
-      // on mouseout, reset the background colour
-      $('#pokedex-logo').css('background-image', "url('img/pokedex-logo-bw.png')");
-      $('#body').css('background-image', 'none');
-    });
+  $('#pokedex_logo').hover(function() {
+      $('#pokedex_logo').css('background-image', "url('img/pokedex_logo.png')");
+      $('#body').css('background-image', "url('img/pokeball_paper.png')");
+      $('#body').css('background-position', "center");
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#pokedex_logo').css('background-image', "url('img/pokedex_logo_bw.png')");
+    $('#body').css('background-image', 'none');
   });
+});
+
+
+// Home
+
+// open modal
+$('#table tbody').on('click', 'tr', function () {
+  $('#modal').css("display", "block");
+});
+
+// close modal
+$('#modal div ').on('click', '.modal_close', function () {
+  $('#modal').css("display", "none");
+} );
