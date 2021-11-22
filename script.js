ee193f17a790d5237d9a2b52825306a85f4a3f1e
rose <rosedorleans1@gmail.com>
2021-11-22 11:45:05 +0100

    feat: modal changes

--- script.js
+++ script.js
@@ -20,12 +20,15 @@ $(document).ready(function() {
 
   // open modal
   $('.grid_container').on('click', '.grid_item', function () {
-    $('#modal').css("display", "block");
+    $('#modal').css("display", "block")
+    $("body").addClass("modal-open")
   });
 
   // close modal
   $('#modal').on('click', '.modal_close', function () {
-    $('#modal').css("display", "none");
+    $('#modal').css("display", "none")
+    $("body").removeClass("modal-open")
   } );
 
 });
+
